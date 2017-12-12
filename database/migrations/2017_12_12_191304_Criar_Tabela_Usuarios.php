4eb4ca7dbe0024ff158682eb77a9385aee18d3bf
Michel Oliveira <michel.s.oliveira@gmail.com>
2017-12-12 18:12:52 -0200

    Criação da migration Usuarios
    Criação da migration Criar_Tabela_Usuarios para a tabela usuarios

---
+++ database/migrations/2017_12_12_191304_Criar_Tabela_Usuarios.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CriarTabelaUsuarios extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->increments('id_usuario');
+            $table->string('email')->unique();
+            $table->string('nomeusuario')
+            $table->string('senha');
+            $table->integer('grupo_id')->unsigned();
+            $table->rememberToken();
+            $table->timestamps();
+
+            $table->foreign('grupo_id')->references('id_grupo')->on('produtos')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::dropIfExists('usuarios');
+    }
+}
