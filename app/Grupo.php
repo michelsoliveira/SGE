56bc7df3b7f8534532c04473556d374f97aa1cc3
Michel Oliveira <michel.s.oliveira@gmail.com>
2017-12-13 11:57:03 -0200

    update model grupo
    criação das funções fillable e o relacionamento grupo-permissao do model

--- app/Grupo.php
+++ app/Grupo.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Grupo extends Model
 {
     //
+
+    protected $fillable ['nome', 'descricao', 'id_permissao'];
+
+    public function Permissao()
+    {
+        return $this->hasMany('app\Permissao', 'id_permissao', 'id_permissao');
+    }
+
+
 }
