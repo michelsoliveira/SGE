8aeed569dab32331f14a88c8e5549683cdbaaaf0
Michel Oliveira <michel.s.oliveira@gmail.com>
2017-12-13 12:00:09 -0200

    update model
    criação das funções fillable e relacionamento usuario-grupo

--- app/Usuario.php
+++ app/Usuario.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Usuario extends Model
 {
     //
+    protected $fillable = ['nome', 'email', 'usuario', 'senha', 'id_grupo'];
+
+    public function Grupo()
+    {
+        return $this->hasOne('app\Grupo','id_grupo','id_grupo');
+    }
+
+
 }
