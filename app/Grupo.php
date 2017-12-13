a19f7eb63e998ad420e4692a36804186657827ba
Michel Oliveira <michel.s.oliveira@gmail.com>
2017-12-13 17:25:01 -0200

    update model grupo
    criação da referencia para a tabela grupos

--- app/Grupo.php
+++ app/Grupo.php
@@ -8,12 +8,12 @@ class Grupo extends Model
 {
     //
 
-    protected $fillable ['nome', 'descricao', 'id_permissao'];
+    protected $fillable = ['nome', 'descricao', 'id_permissao'];
 
     public function Permissao()
     {
         return $this->hasMany('app\Permissao', 'id_permissao', 'id_permissao');
     }
 
-
+    protected $table = 'grupos';
 }
