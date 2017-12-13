c493f6679c9d896d474c0259407ccdc747bf802d
Michel Oliveira <michel.s.oliveira@gmail.com>
2017-12-13 17:26:30 -0200

    update usuario model
    criação da referencia da tabela usuarios, metodo para conferencia da senha e referencia da PK da tabela usuarios

--- app/Usuario.php
+++ app/Usuario.php
@@ -11,12 +11,23 @@ class Usuario extends Authenticatable
     //
     use Notifiable;
 
-    protected $fillable = ['nome', 'email', 'usuario', 'senha', 'id_grupo'];
+    protected $fillable = ['nome', 'email', 'usuario', 'password', 'id_grupo'];
+
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
 
     public function Grupo()
     {
         return $this->hasOne('app\Grupo','id_grupo','id_grupo');
     }
 
+    public function setPasswordAttribute($password)
+    {
+        $this->attributes['password'] = bcrypt($password);
+    }
 
+    protected $table = 'usuarios';
+
+    protected $primaryKey = 'id_usuario';
 }
